6efb16a425b2317641fda1721e9635355f4f3378
Kumaran Rajendhiran <kumaran@airt.ai>
2024-06-20 14:40:27 +0530

    Add prisma generate command

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+cat <<< "$CLIENT_SECRET" > client_secret.json
+
+# ToDo: Uncomment the below line once we have project specific migrations
+# prisma migrate deploy
+prisma generate
+
 uvicorn google_sheets.app:app --workers 2 --host 0.0.0.0 --proxy-headers
