b332a048cd65d49c836dd06d4c4d190ff20ff995
rjambrecic <32619626+rjambrecic@users.noreply.github.com>
2024-06-24 13:10:04 +0200

    Remove connection to the captn wasp db (#21)

--- google_sheets/db_helpers.py
+++ google_sheets/db_helpers.py
@@ -23,12 +23,3 @@ async def get_db_connection(
         yield db
     finally:
         await db.disconnect()
-
-
-def get_wasp_db_url() -> str:
-    curr_db_url = environ.get("DATABASE_URL")
-    wasp_db_name = environ.get("WASP_DB_NAME", "waspdb")
-    wasp_db_url = curr_db_url.replace(curr_db_url.split("/")[-1], wasp_db_name)  # type: ignore[union-attr]
-    if "connect_timeout" not in wasp_db_url:
-        wasp_db_url += "?connect_timeout=60"
-    return wasp_db_url
