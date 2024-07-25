fc9a9c47ad3536b58eff5f0a1a8ab7d3d1d44fd8
Robert Jambrecic <robert@airt.ai>
2024-07-25 08:42:50 +0000

    Update response message if dataframe contains Issues column

--- google_sheets/model.py
+++ google_sheets/model.py
@@ -7,3 +7,8 @@ class GoogleSheetValues(BaseModel):
     values: List[List[Any]] = Field(
         ..., title="Values", description="Values to be written to the Google Sheet."
     )
+    issues_present: bool = Field(
+        default=False,
+        title="Issues Present",
+        description="Whether any issues are present.",
+    )
